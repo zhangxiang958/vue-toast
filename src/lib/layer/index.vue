06f7f421f69be82edaf99d90b86fd154e1eca5f5
zhangxiang958 <958033967@qq.com>
2017-09-04 11:31:42 +0800

    fixed bug

--- src/lib/layer/index.vue
+++ src/lib/layer/index.vue
@@ -20,7 +20,6 @@
         name="layer"
     >
     <div 
-        class="loading" 
          key="layer" 
          v-if="show">
         <div class="loadingMsg" 
@@ -62,13 +61,6 @@
 </template>
 
 <style scoped>
-    .loading {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
     .mask {
         position: absolute;
         top: 0;
